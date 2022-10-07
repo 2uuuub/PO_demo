c2ea2c7e80eac550894e393e4ea1f57c9c268f50
2uuuub <384036208@qq.com>
2022-10-07 21:58:19 +0800

    Changes002

--- 用例/多进程运行.py
+++ 用例/多进程运行.py
@@ -6,8 +6,12 @@
 
 import os
 
-# ver = os.system("pytest test_rule_buy.py::MyTestClass::test_1_tichucaimaixuqiu --demo_mode")
+path = os.getcwd()  # 获取当前路径
+os.system(f"pytest {path}/ --html=report.html -n=2")  # -n为多进程
+# ver = os.system("pytest test_rule_buy.py::MyTestClass::test_1_tichucaimaixuqiu --demo_mode")  # 执行指定用例
 # ver = os.system("pytest test_rule_buy.py::RuleBuy::test_8_Submit_application --pdb -s")
-ver = os.system("pytest D:/测试资料整理/自动化项目整理/PO模型/用例/ --report=自动化报告.html --title=自动化测试报告 --tester=杨靖锋 "
-                "--desc=qq首页登录测试  --template=1 -n=2")  #-n为多线程/多进程
+# ver = os.system("pytest D:/测试资料整理/自动化项目整理/PO模型/用例/ --report=自动化报告.html --title=自动化测试报告 --tester=杨靖锋 "
+#                 "--desc=qq首页登录测试  --template=1 -n=2")  # -n为多线程/多进程
 # ver = os.system("pytest D:/测试资料整理/自动化项目整理/PO模型/用例/ --html=report.html")
+
+
